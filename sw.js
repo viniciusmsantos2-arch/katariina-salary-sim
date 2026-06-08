1dbd242b248534e9e9448dc9591525d6523d6c62
viniciusmsantos2-arch <viniciusmsantos2@gmail.com>
2026-06-08 14:37:38 +0300

    Add contract type (80/100%), update experience supplements to +5%/+10%, bump SW cache to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'salary-sim-v1';
+const CACHE = 'salary-sim-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
