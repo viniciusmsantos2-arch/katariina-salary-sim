7f1a5c35fd8f56ed485a71820c54532882386daa
viniciusmsantos2-arch <viniciusmsantos2@gmail.com>
2026-06-08 14:48:50 +0300

    Fix 80% contract: hourly rate always from full base; split fullCalcBase/baseReceived; bump SW v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'salary-sim-v2';
+const CACHE = 'salary-sim-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
