9b426eb2bd4242578052a8526a64b3ed32bfe799
viniciusmsantos2-arch <viniciusmsantos2@gmail.com>
2026-06-13 14:24:33 +0300

    Add 3 new add-ons (oncall/placement/personal), shorten motivational message; SW v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'salary-sim-v6';
+const CACHE = 'salary-sim-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
